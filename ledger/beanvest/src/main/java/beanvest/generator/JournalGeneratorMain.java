b53c2118b0438300208e1d8a43b1135af5577cf3
bb <beanvestgh+bb@gmail.com>
2023-07-19 12:59:07 +0100

    extract JournalGenerator out of the Main class

--- ledger/beanvest/src/main/java/beanvest/generator/JournalGeneratorMain.java
+++ ledger/beanvest/src/main/java/beanvest/generator/JournalGeneratorMain.java
@@ -1,46 +1,16 @@
 package beanvest.generator;
 
 import java.io.IOException;
-import java.time.LocalDate;
-import java.util.List;
-
-import static beanvest.generator.AccountOperationGenerator.Interval.*;
-import static beanvest.generator.AccountOperationGenerator.Operation.*;
 
 /**
  * Generates sample journals
  */
 public class JournalGeneratorMain {
-
-
     public static void main(String[] args) throws IOException {
-        var journalGenerator = new JournalGeneratorMain();
+        var journalGenerator = new JournalGenerator();
         var journalWriter = new JournalWriter();
 
         var accounts = journalGenerator.generateJournals();
         journalWriter.writeToFiles(args[0], accounts);
-    }
-
-    private List<AccountJournalWriter> generateJournals() {
-        var trading = new AccountJournalWriter("trading");
-        var savings = new AccountJournalWriter("saving:savings");
-        var regularSaver = new AccountJournalWriter("saving:regularSaver");
-
-        var start = LocalDate.parse("2023-01-01");
-        var end = LocalDate.parse("2024-01-01");
-
-        var generator = new AccountOperationGenerator(start, end, trading);
-        generator.generate("930", DEPOSIT, MONTHLY);
-        generator.generate("10", FEE, QUARTERLY);
-
-        generator = new AccountOperationGenerator(start, end, savings);
-        generator.generate("180", DEPOSIT, MONTHLY);
-        generator.generate("20", WITHDRAW, QUARTERLY);
-        generator.generate("10", INTEREST, MONTHLY);
-
-        generator = new AccountOperationGenerator(start, end, regularSaver);
-        generator.generate("100", DEPOSIT, MONTHLY);
-        generator.generate("15", INTEREST, MONTHLY);
-        return List.of(savings, trading, regularSaver);
     }
 }
