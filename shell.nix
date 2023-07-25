fe0684f694cfdd2aa46eb7b8df686f2c4c90a0e5
bb <beanvestgh+bb@gmail.com>
2023-07-25 20:29:10 +0100

    add missing GRAALVM_HOME to the nix env

--- shell.nix
+++ shell.nix
@@ -16,6 +16,8 @@ pkgs.mkShell {
   shellHook =
     ''
       export JAVA_HOME=/nix/store/i0a2rrjxhawark7wrf6hh0rkipfdwmk6-graalvm19-ce-22.3.1
+      export GRAALVM_HOME=/nix/store/i0a2rrjxhawark7wrf6hh0rkipfdwmk6-graalvm19-ce-22.3.1
+
     '';
 }
 
