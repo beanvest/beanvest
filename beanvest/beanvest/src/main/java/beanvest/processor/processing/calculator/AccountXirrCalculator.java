8bb70d21a5c28944607a954e16a5056eb24f023e
bb <beanvestgh+bb@gmail.com>
2023-08-02 09:27:56 +0100

    extract duplicated xirr calculation

--- beanvest/beanvest/src/main/java/beanvest/processor/processing/calculator/AccountXirrCalculator.java
+++ beanvest/beanvest/src/main/java/beanvest/processor/processing/calculator/AccountXirrCalculator.java
@@ -2,26 +2,15 @@ package beanvest.processor.processing.calculator;
 
 import beanvest.processor.processing.collector.FullCashFlowCollector;
 import beanvest.result.Result;
-import beanvest.journal.CashFlow;
-import beanvest.result.ErrorFactory;
 import beanvest.result.UserErrors;
-import org.decampo.xirr.NonconvergenceException;
-import org.decampo.xirr.OverflowException;
-import org.decampo.xirr.Transaction;
-import org.decampo.xirr.Xirr;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
-import static java.util.stream.Collectors.toList;
 
 public class AccountXirrCalculator {
     private final FullCashFlowCollector fullCashFlowCollector;
     private final TotalValueCalculator totalValueCalculator;
+    private final XirrCalculator xirrCalculator = new XirrCalculator();
 
     public AccountXirrCalculator(FullCashFlowCollector fullCashFlowCollector, TotalValueCalculator totalValueCalculator) {
         this.fullCashFlowCollector = fullCashFlowCollector;
@@ -33,39 +22,6 @@ public class AccountXirrCalculator {
         if (totalValueResult.hasError()) {
             return totalValueResult;
         }
-        var xirrTransactions = convertToXirrTransactions(
-                fullCashFlowCollector.get(), totalValueResult.getValue(),
-                endDate);
-
-        return xirrTransactions.size() >= 2
-                ? calculateStats(xirrTransactions)
-                : Result.failure(ErrorFactory.xirrNoTransactions());
-    }
-
-    private Result<BigDecimal, UserErrors> calculateStats(List<Transaction> transactions) {
-        try {
-            return Result.success(BigDecimal.valueOf(Xirr.builder().withGuess(10).withTransactions(transactions).xirr()));
-        } catch (NonconvergenceException | OverflowException e) {
-            return Result.failure(ErrorFactory.xirrCalculationsFailed());
-        }
-    }
-
-    private List<Transaction> convertToXirrTransactions(List<CashFlow> relevantCashFlows, BigDecimal endingAccountValue, LocalDate endDate) {
-        var transactionsFromJournal = relevantCashFlows.stream()
-                .filter(cf -> cf.transferredAmount().amount().abs().compareTo(BigDecimal.ZERO) != 0)
-                .map(cf -> new Transaction(cf.transferredAmount().negate().amount().doubleValue(), cf.date())).toList();
-        var xirrTransactions = new ArrayList<>(transactionsFromJournal);
-        if (endingAccountValue.compareTo(BigDecimal.ZERO) != 0) {
-            xirrTransactions.add(new Transaction(endingAccountValue.doubleValue(), endDate));
-        }
-        return mergeTransactionsOnTheSameDay(xirrTransactions).collect(toList());
-    }
-
-    private Stream<Transaction> mergeTransactionsOnTheSameDay(List<Transaction> xirrTransactions) {
-        return xirrTransactions.stream()
-                .collect(Collectors.toMap(Transaction::getWhen, Transaction::getAmount, Double::sum))
-                .entrySet()
-                .stream()
-                .map(e -> new Transaction(e.getValue(), e.getKey()));
+        return xirrCalculator.calculateXirr(endDate, fullCashFlowCollector.get(), totalValueResult.getValue());
     }
 }
--- beanvest/beanvest/src/main/java/beanvest/processor/processing/calculator/XirrPeriodicCalculator.java
+++ beanvest/beanvest/src/main/java/beanvest/processor/processing/calculator/XirrPeriodicCalculator.java
@@ -4,22 +4,11 @@ import beanvest.journal.CashFlow;
 import beanvest.journal.Value;
 import beanvest.processor.processing.AccountType;
 import beanvest.processor.processing.collector.PeriodCashFlowCollector;
-import beanvest.result.ErrorFactory;
 import beanvest.result.Result;
 import beanvest.result.UserErrors;
-import org.decampo.xirr.NonconvergenceException;
-import org.decampo.xirr.OverflowException;
-import org.decampo.xirr.Transaction;
-import org.decampo.xirr.Xirr;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
-import static java.util.stream.Collectors.toList;
 
 public class XirrPeriodicCalculator {
     private final PeriodCashFlowCollector fullCashFlowCollector;
@@ -28,6 +17,7 @@ public class XirrPeriodicCalculator {
     private final AccountType accountType;
     private Result<BigDecimal, UserErrors> previousValue = Result.success(BigDecimal.ZERO);
     private LocalDate previousDate = LocalDate.MIN;
+    private XirrCalculator xirrCalculator = new XirrCalculator();
 
     public XirrPeriodicCalculator(PeriodCashFlowCollector fullCashFlowCollector, TotalValueCalculator totalValueCalculator, HoldingsValueCalculator holdingsValueCalculator, AccountType accountType) {
         this.fullCashFlowCollector = fullCashFlowCollector;
@@ -44,13 +34,7 @@ public class XirrPeriodicCalculator {
 
         var relevantCashFlows = fullCashFlowCollector.get();
         relevantCashFlows.add(0, new CashFlow(previousDate, Value.of(previousValue.getValue(), "GBP")));
-        var xirrTransactions = convertToXirrTransactions(
-                relevantCashFlows, totalValueResult.getValue(),
-                endDate);
-
-        var result = xirrTransactions.size() >= 2
-                ? calculateStats(xirrTransactions)
-                : Result.<BigDecimal, UserErrors>failure(ErrorFactory.xirrNoTransactions());
+        var result = xirrCalculator.calculateXirr(endDate, relevantCashFlows, totalValueResult.getValue());
 
         previousValue = totalValueResult;
         previousDate = endDate;
@@ -63,32 +47,5 @@ public class XirrPeriodicCalculator {
         } else {
             return totalValueCalculator.calculateValue(endDate, targetCurrency);
         }
-    }
-
-    private Result<BigDecimal, UserErrors> calculateStats(List<Transaction> transactions) {
-        try {
-            return Result.success(BigDecimal.valueOf(Xirr.builder().withGuess(10).withTransactions(transactions).xirr()));
-        } catch (NonconvergenceException | OverflowException e) {
-            return Result.failure(ErrorFactory.xirrCalculationsFailed());
-        }
-    }
-
-    private List<Transaction> convertToXirrTransactions(List<CashFlow> relevantCashFlows, BigDecimal endingAccountValue, LocalDate endDate) {
-        var transactionsFromJournal = relevantCashFlows.stream()
-                .filter(cf -> cf.transferredAmount().amount().abs().compareTo(BigDecimal.ZERO) != 0)
-                .map(cf -> new Transaction(cf.transferredAmount().negate().amount().doubleValue(), cf.date())).toList();
-        var xirrTransactions = new ArrayList<>(transactionsFromJournal);
-        if (endingAccountValue.compareTo(BigDecimal.ZERO) != 0) {
-            xirrTransactions.add(new Transaction(endingAccountValue.doubleValue(), endDate));
-        }
-        return mergeTransactionsOnTheSameDay(xirrTransactions).collect(toList());
-    }
-
-    private Stream<Transaction> mergeTransactionsOnTheSameDay(List<Transaction> xirrTransactions) {
-        return xirrTransactions.stream()
-                .collect(Collectors.toMap(Transaction::getWhen, Transaction::getAmount, Double::sum))
-                .entrySet()
-                .stream()
-                .map(e -> new Transaction(e.getValue(), e.getKey()));
     }
 }
