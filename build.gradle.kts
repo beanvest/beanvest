40e5daaeeebd3f2355e1b176e009d465e5b3508f
bb <beanvestgh+bb@gmail.com>
2023-08-11 13:32:51 +0100

    bundle test-build-generate into one gradle task 'all' to speed up pre-commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,3 +17,31 @@ allprojects {
         }
     }
 }
+tasks.register("generate") {
+    dependsOn(":beanvest:scripts:generateSampleJournal",
+            ":beanvest:scripts:generateUsageDoc",
+            ":beanvest:scripts:generateSampleJson")
+    group = "generation"
+    description = "Regenerates everything"
+}
+tasks.register("nativeGenerate") {
+    dependsOn(":beanvest:scripts:generateSampleJournal",
+            ":beanvest:scripts:generateUsageDocNative",
+            ":beanvest:scripts:generateSampleJsonNative")
+    group = "generation"
+    description = "Regenerates everything using native build"
+}
+
+tasks.register("all") {
+    group = "all"
+    description = "build, test and generate."
+    dependsOn("build", "test", "generate")
+}
+tasks.register("nativeAll") {
+    group = "all"
+    description = "build, test and generate using native binaries."
+    dependsOn("build",
+            ":beanvest:acceptance:nativeTest",
+            ":beanvest:beanvest:nativeTest",
+            "nativeGenerate")
+}
