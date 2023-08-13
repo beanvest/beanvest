d12f3e633a7751534831a7b81691e57fd4f4f473
bb <beanvestgh+bb@gmail.com>
2023-08-13 01:33:12 +0100

    UI: print accounts and some numbers

--- beanvest/ui/src/lib/beanvest/Report.svelte
+++ beanvest/ui/src/lib/beanvest/Report.svelte
@@ -1,19 +1,44 @@
 <script>
     const apiURL = "http://localhost:5173/sample1.json";
 
+    /** @type {PortfolioStatsDto2} */
+    report;
+
     $: report = null;
 
-    async function getData() {
+    async function fetchReport() {
         const response = await fetch(apiURL);
-        let newVar = await response.json();
-        report = newVar;
+
+        report = await response.json();
     }
+
 
 </script>
 
-<div on:click={getData}>load report</div>
+<div on:click={fetchReport}>load report</div>
 {#if report }
-    {#each report.accounts as account}
-        <div>{account}</div>
-    {/each}
+    <table>
+        <tr>
+            <th>accounts</th>
+            {#each report.periods as period}
+                <th>{period.start}</th>
+            {/each}
+        </tr>
+        {#each report.accountDtos as accountDto}
+            <tr><td>{accountDto.account}</td>
+                {#each Object.entries(accountDto.periodStats) as [periodTitle, stats]}
+                    {#each Object.entries(stats.stats) as [statName, value]}
+                    <td class="value">{value.value.toFixed(0)}</td>
+                        {/each}
+                {/each}
+            </tr>
+
+        {/each}
+    </table>
 {/if}
+
+<style>
+    td.value {
+        text-align: right;
+    }
+</style>
