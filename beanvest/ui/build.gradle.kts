feefa5fa409724fb78d0a729efb4722558a85ccb
bb <beanvestgh+bb@gmail.com>
2023-08-16 07:38:05 +0100

    fix npm install not ran before npm test:unit

--- beanvest/ui/build.gradle.kts
+++ beanvest/ui/build.gradle.kts
@@ -5,16 +5,18 @@ group = "beanvest.ui"
 val npm = "/nix/store/1gnvy2dhh311c900hzyw6ppjdhnir2s5-nodejs-20.5.0/bin/npm";
 
 tasks.register("jsInstall") {
-    exec {
-        commandLine("$npm", "install")
+    doLast {
+        exec {
+            commandLine("$npm", "install")
+        }
     }
 }
 
 tasks.register("jsTest") {
     dependsOn("jsInstall")
-
-    exec {
-        val npm = "/nix/store/1gnvy2dhh311c900hzyw6ppjdhnir2s5-nodejs-20.5.0/bin/npm";
-        commandLine("$npm", "run", "test:unit", "--", "--run")
+    doLast {
+        exec {
+            commandLine("$npm", "run", "test:unit", "--", "--run")
+        }
     }
 }
