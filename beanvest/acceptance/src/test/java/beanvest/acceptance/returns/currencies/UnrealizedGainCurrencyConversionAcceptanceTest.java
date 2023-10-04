0523824002ae614f32825f480961e599d5b03e3d
bb <beanvestgh+bb@gmail.com>
2023-10-04 13:10:32 +0100

    polish tests for unrealized gains with currency conversion

--- beanvest/acceptance/src/test/java/beanvest/acceptance/returns/currencies/UnrealizedGainCurrencyConversionAcceptanceTest.java
+++ beanvest/acceptance/src/test/java/beanvest/acceptance/returns/currencies/UnrealizedGainCurrencyConversionAcceptanceTest.java
@@ -4,14 +4,15 @@ import beanvest.acceptance.returns.ReturnsDsl;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
-@Disabled("TODO")
+@Disabled
 public class UnrealizedGainCurrencyConversionAcceptanceTest {
     protected final ReturnsDsl dsl = new ReturnsDsl();
 
     @Test
     void unrealizedGainIsBasedOnCommodityGain() {
         dsl.setCurrency("PLN");
-        dsl.setColumns("value");
+        dsl.setColumns("ugain");
+        dsl.setEnd("2021-01-10");
 
         dsl.runCalculateReturns("""
                 account trading
@@ -21,15 +22,17 @@ public class UnrealizedGainCurrencyConversionAcceptanceTest {
                 2021-01-02 deposit 1
                 2021-01-03 buy 1 X for 1
                 2021-01-04 price X 2 GBP
+                2021-01-04 price GBP 7 PLN
                 """);
 
-        dsl.verifyValue("trading", "TOTAL", "5");
+        dsl.verifyUnrealizedGains("trading", "TOTAL", "5");
     }
 
     @Test
     void unrealizedGainIsNotBasedOnCurrencyGain() {
         dsl.setCurrency("PLN");
-        dsl.setColumns("value");
+        dsl.setColumns("ugain");
+        dsl.setEnd("2021-01-10");
 
         dsl.runCalculateReturns("""
                 account trading
@@ -38,9 +41,10 @@ public class UnrealizedGainCurrencyConversionAcceptanceTest {
                 2021-01-01 price GBP 5 PLN
                 2021-01-02 deposit 1
                 2021-01-03 buy 1 X for 1
+                2021-01-04 price X 1 GBP
                 2021-01-04 price GBP 6 PLN
                 """);
 
-        dsl.verifyValue("trading", "TOTAL", "0");
+        dsl.verifyUnrealizedGains("trading", "TOTAL", "0");
     }
 }
