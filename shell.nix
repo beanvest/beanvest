953a3300ce9d311d7a57002fc6ba66917a7026ba
bb <beanvestgh+bb@gmail.com>
2023-10-05 17:23:48 +0100

    add output path to PATH in nix shell

--- shell.nix
+++ shell.nix
@@ -18,6 +18,7 @@ pkgs.mkShell {
     ''
       export JAVA_HOME=/nix/store/i0a2rrjxhawark7wrf6hh0rkipfdwmk6-graalvm19-ce-22.3.1
       export GRAALVM_HOME=/nix/store/i0a2rrjxhawark7wrf6hh0rkipfdwmk6-graalvm19-ce-22.3.1
+      export PATH=$(pwd)/beanvest/beanvest/build/native/nativeCompile:$PATH
 
     '';
 }
